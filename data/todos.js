6e1f9a57c4327a852cf720237a820905c9eb8cd5
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-18 14:09:49 -0700

    database scripts up and running with endpoint working correctly

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'clean room',
+    completed: true
+  },
+  {
+    id: 2,
+    todo: 'clean car',
+    completed: false
+  },
+  {
+    id: 3,
+    todo: 'pick fruit',
+    completed: true
+  }
+];
